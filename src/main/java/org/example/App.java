217b7631549730df55da5295ab81c26f957b853f
Kirill <larkin.kirill@mail.ru>
2024-12-11 18:36:55 +0300

    first commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -17,7 +17,6 @@ public class App {
                 "</body>\n" +
                 "</html>";
 
-
         String filePath = "target/site/index.html";
 
         try (FileWriter writer = new FileWriter(filePath)) {
